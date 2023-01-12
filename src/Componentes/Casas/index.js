df790653497917023f84615c4c61d44779318da4
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-01-12 00:41:38 -0300

    Escondendo casas vazias

--- src/Componentes/Casas/index.js
+++ src/Componentes/Casas/index.js
@@ -3,7 +3,7 @@ import './Casas.css'
 
 const Casas = (props) => {
     return (
-        <section className='Casas' style={{backgroundImage:`url(${props.fundo})`}}>
+        (props.alunos.length > 0) && <section className='Casas' style={{backgroundImage:`url(${props.fundo})`}}>
             <h3>{props.nome}</h3>
             <div className='aluno'>
                 {props.alunos.map( aluno => <Aluno nome={aluno.nome} imagem={aluno.imagem}/> )}
