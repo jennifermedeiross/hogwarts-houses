17651befd33038e049e9be5c2d456483b10dab3d
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-01-12 01:15:32 -0300

    Chamando cores de fundo

--- src/Componentes/Casas/index.js
+++ src/Componentes/Casas/index.js
@@ -1,12 +1,12 @@
 import Aluno from '../Aluno'
 import './Casas.css'
 
-const Casas = (props) => {
+const Casas = ({nome, alunos, corCabecalho, fundo}) => {
     return (
-        (props.alunos.length > 0) && <section className='Casas' style={{backgroundImage:`url(${props.fundo})`}}>
-            <h3>{props.nome}</h3>
+        (alunos.length > 0) && <section className='Casas' style={{backgroundImage:`url(${fundo})`}}>
+            <h3>{nome}</h3>
             <div className='aluno'>
-                {props.alunos.map( aluno => <Aluno nome={aluno.nome} imagem={aluno.imagem}/> )}
+                {alunos.map( aluno => <Aluno corCabecalho={corCabecalho} nome={aluno.nome} imagem={aluno.imagem}/> )}
             </div>
         </section>
     )
